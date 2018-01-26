9e642d8e02196af738050d3ce33b4629b5574a0f
Aryel Mota Góis <aryel.gois@gmail.com>
2018-01-26 18:43:28 -0300

    Enable production if 'assets.json' exists

--- public/index.php
+++ public/index.php
@@ -1,11 +1,24 @@
+<?php
+if (file_exists('assets/assets.json')) {
+    $mode = 'production';
+    $assets = json_decode(file_get_contents('assets/assets.json'), true);
+} else {
+    $mode = 'development';
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="utf-8">
         <title>PHP + Vue.JS</title>
+<?php if ($mode == 'production'): ?>
+        <link rel="stylesheet" href="/assets/<?= $assets['main']['css'] ?>" />
+<?php endif; ?>
     </head>
     <body>
         <div id="app"></div>
-        <script src="http://localhost:8080/js/main.js"></script>
+        <script src="<?= $mode == 'production'
+            ? '/assets/' . $assets['main']['js']
+            : 'http://localhost:8080/js/main.js' ?>"></script>
     </body>
 </html>
