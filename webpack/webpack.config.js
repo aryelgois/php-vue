2b1333db703c4bdbfabc172b34c73482b25efb81
Aryel Mota Góis <aryel.gois@gmail.com>
2018-01-26 15:50:41 -0300

    Add webpack config

---
+++ webpack/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = function(env = {}) {
+  if (env.production) {
+    process.env.NODE_ENV = 'production';
+  }
+
+  return {
+    entry: './src/main.js',
+    output: {
+      path: path.resolve(__dirname, '../public/assets'),
+      filename: env.production ? 'js/main.min.js' : 'js/main.js'
+    },
+    plugins: env.production ? [
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: '"production"'
+        }
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      }),
+    ] : [
+    ],
+    devtool: env.production ? false : '#cheap-module-eval-source-map'
+  };
+};
