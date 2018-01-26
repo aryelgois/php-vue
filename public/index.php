ebbb980c77078462a53e2c64e5ac6bd7c8576ac0
Aryel Mota Góis <aryel.gois@gmail.com>
2018-01-26 18:31:55 -0300

    Add Hello World example

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>PHP + Vue.JS</title>
+    </head>
+    <body>
+        <div id="app"></div>
+        <script src="http://localhost:8080/js/main.js"></script>
+    </body>
+</html>
---
+++ webpack/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import Hello from './components/Hello.vue';
+
+Vue.component('hello', Hello);
+
+var app = new Vue({
+  el: '#app',
+  render (h) {
+    return h('hello', Hello);
+  }
+});
